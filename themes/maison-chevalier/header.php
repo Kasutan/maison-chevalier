65705e4fe95a130fd2ec3826d3c1d467c82f4141
Kasutan <contact@kasutan.pro>
2020-05-19 19:37:43 +0200

    Header (1/3)

--- themes/maison-chevalier/header.php
+++ themes/maison-chevalier/header.php
@@ -32,22 +32,15 @@
 
 		<div class="site-branding">
 			<?php
-			the_custom_logo();
-			if ( is_front_page() || is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
+			if(has_custom_logo( )) {
+				the_custom_logo();
+			} else {
 				?>
 				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$chevalier_description = get_bloginfo( 'description', 'display' );
-			if ( $chevalier_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $chevalier_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+			<?php
+			}
+			?>
+		</div>
 
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">Menu</button>
